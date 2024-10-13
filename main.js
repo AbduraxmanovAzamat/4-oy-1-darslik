48ba8b8bd96e1913c4144f04719587b4e7f2bc81
AbduraxmanovAzama <azamatabduraxmonov25@gmail.com>
2024-10-14 01:29:19 +0500

    yukladim

---
+++ main.js
@@ -0,0 +1,137 @@
+// 1-masala
+// let a = +prompt("kvadratning tomonini kiriting");
+// console.log(a)
+
+// let p
+
+// p = 4 * a
+
+// console.log("Kvadratning parametri:"  + p);
+
+
+// 2-masala
+// let a = +prompt("Kavadratning tomonini kiriting")
+// console.log(a)
+
+// let s
+
+// s = a * a
+
+// console.log("Kvadratning yuzasi:" + s);
+
+
+
+// 3-masala
+// let a = +prompt("To'g'ri to'rtburchakning tomonini kirting")
+// let b = +prompt("To'g'ri to'rtburchakning tomonini kirting")
+
+// let s
+// s = a*b
+
+// console.log("Uning yuzasi:" + s);
+
+// let p
+// p = 2 * (a + b)
+
+// console.log("Uning parametri:" + p);
+
+
+// 4-masala
+
+// let d = +prompt("Aylaning diametrini kiriting")
+
+// P = 3.14
+
+// let l
+
+// l = P*d
+
+// console.log("Uning uzunligi:" + l);
+
+
+// 5-masala
+
+// let a = +prompt("Kubning yon tomini kirgizing")
+
+// let v
+
+// v = a * a * a
+
+// console.log("Kubning hajmi:" + v)
+
+// let s
+
+// s = 6 * a * a
+
+// console.log("Kubning to'la sirti:" + s)
+
+
+// 6-masala
+
+// let a = +prompt("Paralelepipedning tomonini kiriting")
+// let b = +prompt("Paralelepipedning tomonini kiriting")
+// let c = +prompt("Paralelepipedning tomonini kiriting")
+
+// let v 
+
+// v = a * b * c
+
+// console.log("Paralelepipedning hajmi:" + v);
+
+// let s
+
+// s = 2 * (a*b+b*c+a*c)
+
+// console.log("Paralelepipedning to'la sirti:" + s)
+
+
+
+// 7-masala
+
+// let r = +prompt("Doiraning radiusini kiriting")
+
+// let l 
+
+// P = 3.14
+
+// l = 2 * P * r
+
+// console.log("Doiraning uzunligi:" + l);
+
+// let s
+
+// s = P * r * r
+
+// console.log("Doiraning yuzasi:" + s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
